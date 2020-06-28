14422d0273b56a588fb1ba9a510ed1689c04b368
sandeeptechhome <32474594+sandeeptechhome@users.noreply.github.com>
2020-06-28 17:56:57 +0530

    file name changes

---
+++ app.js
@@ -0,0 +1,27 @@
+$(function() {
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 20000,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1,
+            nav: true,
+          },
+          600: {
+            items: 1,
+            nav: true,
+          },
+          1000: {
+            items: 1,
+            nav: true,
+            //loop: false,
+          },
+        },
+      });
+
+});
