b68c86303f6cd5df4526441516b6dd6dafb752e6
sandeeptechhome <32474594+sandeeptechhome@users.noreply.github.com>
2020-07-04 22:34:29 +0530

    fixes

--- app.js
+++ app.js
@@ -45,23 +45,14 @@ $(function () {
   //   $("#capabilityBtn ul").toggle(500);
   //   console.log("capability clicked")
   // });
-  
-  $('#success').hide()
-  var href = window.location.href;
-  if (href.includes('contact')) {
-    $('#exampleModal').modal('show');
-  }
   $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus');
   });
   $('#success').hide();
-
   function submitForm() {
     let name = $('#user_name').val();
     let email = $('#user_email').val();
     let message = $('#user_message').val();
-
-
     let data = {
       service_id: "gmail",
       template_id: "template_i2YiDv4m",
@@ -72,14 +63,12 @@ $(function () {
         "message": message
       }
     };
-    console.log(data)
-
     $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
       type: 'POST',
       data: JSON.stringify(data),
       contentType: 'application/json'
     }).done(function () {
-      $('#success').addClass('orange-background').show();
+      $('#m-content').addClass('orange-background').show();
       $('#info-submit').hide();
     }).fail(function (error) {
       alert('Oops... ' + JSON.stringify(error));
