5e224152f1f78d51d06b00696b84557691c6c975
devsandeep26 <guntupalli.sandeep567@gmail.com>
2020-07-15 20:01:30 +0530

    fixes

--- app.js
+++ app.js
@@ -66,6 +66,9 @@ $(function () {
   $('#submit-btn').click(function () {
     submitForm();
   });
+  $(".do-list li").hover( function (e) {
+    $(this).toggleClass('animate__animated animate__pulse');
+});
   function submitForm() {
     let name = $('#user_name').val();
     let email = $('#user_email').val();
